a1e7f8dcd0cd5dd27ccc59d1f3c17b9fbf922dde
SantinoPeirano <speirano@mail.austral.edu.ar>
2025-04-14 01:36:50 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,8 @@
 def slice_simple():
-    texto="Awesome".lower()
-
-    print (texto[0:3])
-    print (texto [2:5])
-    print (texto[0:4]+texto[4:7])
+    texto = "Awesome"
+    texto = texto.lower()
+    medio = int(len(texto)/2)
+    print(texto [0:3])
+    print(texto [medio-1:medio+2])
+    print(texto)
     
